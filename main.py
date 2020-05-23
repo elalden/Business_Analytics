aa3dd3abd8de9055d008d4984f515a0ddfa1ccba
elalden-uaex <ealden@uaex.edu>
2020-05-23 17:00:50 -0600

    add requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
-
+import pandas as pd
+from sklearn.cluster import KMeans
 writer = csv.reader('./Static/fob_spring_2019_aggregates.csv')
 with open('./Static/fob_spring_2019_aggregates.csv', newline='') as csvfile:
     data = csv.reader(csvfile, delimiter=' ', quotechar='|')

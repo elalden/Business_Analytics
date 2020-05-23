2c8950a0a70c4740af4f8a109264f9c437180314
elalden-uaex <ealden@uaex.edu>
2020-05-23 17:55:47 -0600

    Add clusters

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import csv
+
+import documents as documents
 import pandas as pd
 from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 writer = csv.reader('./Static/fob_spring_2019_aggregates.csv')
+doc_string = []
 with open('./Static/fob_spring_2019_aggregates.csv', newline='') as csvfile:
-    data = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in data:
-        print(', '.join(row))
+     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     for row in spamreader:
+         doc_string.append(str(row))
+
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(doc_string)
+
+true_k = 2
+model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=3)
+model.fit(X)
+
+print("Top terms per cluster:")
+order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizer.get_feature_names()
+for i in range(true_k):
+    print("Cluster %d:" % i),
+    for ind in order_centroids[i, :10]:
+        print(' %s' %terms[ind]),
+    print
+
+

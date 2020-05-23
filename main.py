6456ec48aee3e5f2cff33ec29e8ff32463f6f0c8
elalden-uaex <ealden@uaex.edu>
2020-05-23 16:50:51 -0600

    read csv

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import csv
+
+writer = csv.reader('./Static/fob_spring_2019_aggregates.csv')
+with open('./Static/fob_spring_2019_aggregates.csv', newline='') as csvfile:
+    data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in data:
+        print(', '.join(row))
